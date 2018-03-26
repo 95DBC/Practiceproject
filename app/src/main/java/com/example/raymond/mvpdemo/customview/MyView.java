7c8a9f0998bebea6c0c401242d881c2f5e0e9012
95DBC <171291124@qq.com>
2018-03-26 10:14:12 +0800

    解决文件没法识别v7 包的问题

---
+++ app/src/main/java/com/example/raymond/mvpdemo/customview/MyView.java
@@ -0,0 +1,30 @@
+package com.example.raymond.mvpdemo.customview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Raymond 陈徐锋 on 2018/3/26.
+ * Email: raymond@hinteen.com
+ * Description:
+ */
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+}
